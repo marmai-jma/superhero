5c2d8c2d490da97173865e1a937d4640e88fe93f
Stéphane Rigaut <stephane.rigaut@bnpparibas.com>
2019-10-22 13:32:14 +0200

    removing EnemyRepository

--- src/main/java/com/bnpparibas/itg/superhero/superhero/restjpa/EnemyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bnpparibas.itg.superhero.superhero.restjpa;
-
-import com.bnpparibas.itg.superhero.superhero.mapping.Enemy;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(path = "enemies")
-public interface EnemyRepository extends CrudRepository<Enemy, Long> {}
