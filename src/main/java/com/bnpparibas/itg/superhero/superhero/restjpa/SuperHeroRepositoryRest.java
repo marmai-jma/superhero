23eadd8cfabad07ed9ee5e67258d65ebf9ec050e
Stéphane Rigaut <stephane.rigaut@bnpparibas.com>
2019-10-23 10:00:17 +0200

    init with superhero

--- src/main/java/com/bnpparibas/itg/superhero/superhero/restjpa/SuperHeroRepositoryRest.java
+++ src/main/java/com/bnpparibas/itg/superhero/superhero/restjpa/SuperHeroRepositoryRest.java
@@ -1,7 +1,7 @@
 package com.bnpparibas.itg.superhero.superhero.restjpa;
 
 import com.bnpparibas.itg.superhero.superhero.mapping.SuperHero;
-import org.springframework.data.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(path = "superheros")
