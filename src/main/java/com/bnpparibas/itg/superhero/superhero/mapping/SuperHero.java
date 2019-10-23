5b4069ea293bcb1316ac117cd6124cad29c71f65
Stephhhh <stephh.r@gmail.com>
2019-10-23 09:52:39 +0200

    Update SuperHero.java

--- src/main/java/com/bnpparibas/itg/superhero/superhero/mapping/SuperHero.java
+++ src/main/java/com/bnpparibas/itg/superhero/superhero/mapping/SuperHero.java
@@ -1,9 +1,11 @@
 package com.bnpparibas.itg.superhero.superhero.mapping;
 
+import lombok.Getter;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@Getter
 @Entity
 public class SuperHero {
 
