30f0cf0c7bfebdf14e55b501ab6008a74e3c1fb9
Stephhhh <stephh.r@gmail.com>
2019-10-23 09:51:58 +0200

    Update and rename SuperHeroRepository.java to SuperHeroRepositoryRest.java

--- src/main/java/com/bnpparibas/itg/superhero/superhero/restjpa/SuperHeroRepositoryRest.java
+++ src/main/java/com/bnpparibas/itg/superhero/superhero/restjpa/SuperHeroRepositoryRest.java
@@ -1,8 +1,8 @@
 package com.bnpparibas.itg.superhero.superhero.restjpa;
 
 import com.bnpparibas.itg.superhero.superhero.mapping.SuperHero;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(path = "superheros")
-public interface SuperHeroRepository extends CrudRepository<SuperHero, Long> {}
+public interface SuperHeroRepositoryRest extends JpaRepository<SuperHero, Long> {}
